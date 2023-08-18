be36a9ae448693dff0b1f33af39a22e56fc9e01e
stepanbobrik <stepabobrik42@yandex.by>
2023-08-18 21:53:42 +0300

    solution

---
+++ 10.py
@@ -0,0 +1,9 @@
+def maxSum(nums):
+    sum = 0
+    min_sum = nums[0]
+    max_sum = 0
+    for n in nums:
+        sum+=n
+        max_sum = max(max_sum,sum-min_sum)
+        min_sum =min(sum,min_sum)
+    return max_sum
