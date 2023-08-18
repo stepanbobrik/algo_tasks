dbe67c0b2ebe8d38cfee15f9da19eedac489c3e4
stepanbobrik <stepabobrik42@yandex.by>
2023-08-18 18:34:39 +0300

    solution

---
+++ 2.py
@@ -0,0 +1,13 @@
+def sort(arr):
+    l = 0
+    r = len(arr) - 1
+    while r >= 0:
+        m = [arr[l], l]
+        while l <= r:
+            if arr[l] > m[0]:
+                m = [arr[l], l]
+            l += 1
+        l = 0
+        arr[r], arr[m[1]] = arr[m[1]], arr[r]
+        r -= 1
+    return arr
