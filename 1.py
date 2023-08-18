1294b76d99438f622ab0b86b8ecf15f98bbcceb9
stepanbobrik <stepabobrik42@yandex.by>
2023-08-18 18:15:50 +0300

    solution

--- 1.py
+++ 1.py
@@ -1,4 +1,10 @@
 def sort(arr):
-    for i in range(1,len(arr)):
-        if arr[i] < arr[i-1]:
-            arr[i],arr[i-1] = arr[i-1],arr[i]
+    swapped = 1
+    while swapped:
+        swapped = 0
+        for i in range(1,len(arr)):
+            if arr[i] < arr[i-1]:
+                arr[i],arr[i-1] = arr[i-1],arr[i]
+                swapped = 1
+
+
