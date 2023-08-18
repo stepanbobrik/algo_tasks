3c3d63ecb77cebf5afdc09cacdc9ae8a1b47f54c
stepanbobrik <stepabobrik42@yandex.by>
2023-08-18 21:29:04 +0300

    solution

---
+++ 9.py
@@ -0,0 +1,15 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+def reverse(head):
+    prev = None
+
+    while head:
+        temp = head.next
+        head.next = prev
+        prev = head
+        head = temp
+    return prev
+
+
