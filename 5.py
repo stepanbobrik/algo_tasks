8ef385605b68ba8a6f849e988ac91e77f1089a42
stepanbobrik <stepabobrik42@yandex.by>
2023-08-18 19:58:24 +0300

    solution

---
+++ 5.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+def path(root):
+    ans = []
+    l = 0
+    q = [root]
+    while l<len(q):
+        if not q[l]:
+            break
+        ans.append(q[l].val)
+        q.append(q[l].left)
+        q.append(q[l].right)
+        l+=1
+    return ans
+
+
