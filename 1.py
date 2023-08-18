1f836518778dcc3bf18d1f9697824ee52e6f19cc
stepanbobrik <stepabobrik42@yandex.by>
2023-08-18 18:16:54 +0300

    fix

--- 1.py
+++ 1.py
@@ -2,9 +2,10 @@ def sort(arr):
     swapped = 1
     while swapped:
         swapped = 0
-        for i in range(1,len(arr)):
+        for i in range(1, len(arr)):
             if arr[i] < arr[i-1]:
-                arr[i],arr[i-1] = arr[i-1],arr[i]
+                arr[i], arr[i-1] = arr[i-1], arr[i]
                 swapped = 1
+    return arr
 
 
