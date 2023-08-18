4eac2cc4c2e3f0d7a4a69720f43e34156bdbc722
stepanbobrik <stepabobrik42@yandex.by>
2023-08-18 20:12:02 +0300

    fix

--- 5.py
+++ 5.py
@@ -10,11 +10,11 @@ def path(root):
     q = [root]
     while l<len(q):
         if not q[l]:
-            break
+            l+=1
+            continue
         ans.append(q[l].val)
         q.append(q[l].left)
         q.append(q[l].right)
         l+=1
     return ans
-
 
